5a2cd5558875effb611fd3bb69338422214df8a4
RLB <20372070+robbyblum@users.noreply.github.com>
2020-03-25 15:06:45 -0400

    rewrote codiffmap.peak_amplitudes
    simplified: just FFT each 2D spectrum once and then do a single sum over 
    a 2D chunk for each peak: either 3x3 or 1x1, depending on grid_bool

--- diffmap/codiffmap.py
+++ diffmap/codiffmap.py
@@ -84,7 +84,7 @@ def get_interesting_columns_all2D(datapath, column_indices, number_of_spectra,
     for spectrum_index in range(1, number_of_spectra + 1):
         new_data = get_interesting_columns(datapath, column_indices,
                                            spectrum_index, n_rows)
-        all_relevant_data[spectrum_index - 1] = new_data
+        all_relevant_data[spectrum_index - 1, :, :] = new_data
     return all_relevant_data
 
 
@@ -400,17 +400,16 @@ def sparsify_staggered(data, Nsparse, N3D, offbool):
 
 def peak_amplitudes(data, peak_list, grid_bool):
     """
-
     This function takes 2D data, with coordinates
     data[spectrum_number][column][time_point], and returns a list of summed
     amplitudes for a grid of 9 points centered around the peak of interest.
     Ordered as they were given in peak_list
-
     """
     # make sure the data are in arrays
     data = np.array(data)
     peak_list = np.array(peak_list)
 
+    Npeaks = peak_list.shape[0]
     if len(data.shape) == 2:
         Nspectra = 1
     else:
@@ -422,45 +421,30 @@ def peak_amplitudes(data, peak_list, grid_bool):
     amp_list = peak_list.copy()
     pcorr_f1 = util.get_phasecorr(N1, 1)
 
-    # we output a peak for each 2d spectrum: peak_amp[spectrum][peak]
+    # we output a peak for each 2d spectrum: peak_amp[spectrum, peak]
     if Nspectra == 1:
-        peak_gridamps = np.full((peak_list.shape[0]), np.nan)
+        peak_gridamps = np.full((Npeaks), np.nan)
     else:
-        peak_gridamps = np.full((Nspectra, peak_list.shape[0]), np.nan)
+        peak_gridamps = np.full((Nspectra, Npeaks), np.nan)
 
     # fill out the grid of data: each spectrum, each peak
     for spectrum in np.arange(Nspectra):
-        for peak_num in np.arange(peak_list.shape[0]):
+        temp_data = data[spectrum, :, :]
+        temp_spec = np.fft.fftshift(np.fft.fft(temp_data, axis=1), axes=1)
+        temp_spec_r = (temp_spec * pcorr_f1).real
+
+        for peak_num in np.arange(Npeaks):
             peak_row = int(peak_list[peak_num, 0])
             peak_col = int(peak_list[peak_num, 1])
 
-            if Nspectra == 1:
-                data_left_t = data[peak_row - 1, :]
-                data_center_t = data[peak_row, :]
-                data_right_t = data[peak_row + 1, :]
-            else:
-                data_left_t = data[spectrum][peak_row - 1, :]
-                data_center_t = data[spectrum][peak_row, :]
-                data_right_t = data[spectrum][peak_row + 1, :]
-
-            data_left_f = np.fft.fftshift(np.fft.fft(data_left_t))
-            data_center_f = np.fft.fftshift(np.fft.fft(data_center_t))
-            data_right_f = np.fft.fftshift(np.fft.fft(data_right_t))
-
-            # phase correct into a real spectrum
-            data_left_fr = (data_left_f * pcorr_f1).real
-            data_center_fr = (data_center_f * pcorr_f1).real
-            data_right_fr = (data_right_f * pcorr_f1).real
-
-            amp = 0
-            amp += grid_bool * np.sum(data_left_fr[peak_col - 1:peak_col + 2])
-            amp += np.sum(data_center_fr[peak_col - grid_bool * 1:
-                                         peak_col + 1 + grid_bool])
-            amp += grid_bool * np.sum(data_right_fr[peak_col - 1:peak_col + 2])
+            amp = np.sum(temp_spec_r[peak_row - grid_bool:
+                                     peak_row + grid_bool + 1,
+                                     peak_col - grid_bool:
+                                     peak_col + grid_bool + 1])
 
             if Nspectra == 1:
                 peak_gridamps[peak_num] = amp
             else:
-                peak_gridamps[spectrum][peak_num] = amp
+                peak_gridamps[spectrum, peak_num] = amp
 
     return peak_gridamps
